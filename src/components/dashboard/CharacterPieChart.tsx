7077abc8b21ddb9921c7afe97aaa398ad8a0c183
Liodakis <mliodakis@travelstaytion.com>
2024-10-01 19:53:07 +0300

    refactor: update pie chart

--- src/components/dashboard/CharacterPieChart.tsx
+++ src/components/dashboard/CharacterPieChart.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import Chart from 'react-apexcharts'
 import { useSelector } from 'react-redux'
 import { CharacterState } from '../../types'
@@ -13,14 +13,14 @@ const CharacterPieChart = () => {
     return characters.reduce((total, character) => total + character.films.length, 0)
   }
 
-  const [totalFilms, setTotalFilms] = useState<number>(0)
   const chartData = useMemo(() => {
     const filmsCount: { [key: string]: number } = {}
 
     characters.forEach((character) => {
       filmsCount[character.name] = character.films.length
     })
-    setTotalFilms(calculateTotalFilms())
+
+    const total = calculateTotalFilms()
 
     return {
       series: Object.values(filmsCount),
@@ -40,25 +40,29 @@ const CharacterPieChart = () => {
         tooltip: {
           y: {
             formatter: (value: number) => {
-              const percentage = ((value / totalFilms) * 100).toFixed(2)
+              const percentage = ((value / total) * 100).toFixed(2)
               return `${value} films (${percentage}%)`
             },
           },
         },
       },
     }
-  }, [characters, totalFilms])
+  }, [characters])
 
   return loading ? (
     <FontAwesomeIcon
       icon={faSpinner}
       size="2x"
       spin
-      style={{ marginBlockStart: '24px', width: 'min-content', alignSelf: 'center' }}
+      style={{
+        marginBlockStart: '24px',
+        width: 'min-content',
+        alignSelf: 'center',
+      }}
     />
   ) : (
     <>
-      <Chart options={'pie'} series={chartData.series} type="pie" height="350" />
+      <Chart options={chartData.options} series={chartData.series} type="pie" height="350" />
       <ExportData characters={characters} />
     </>
   )
